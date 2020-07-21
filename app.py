f30ba7893700203e127df95859e31980538be201
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-07-21 16:01:35 -0700

    added top to frame

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ from playsound import playsound
 WINDOW_TITLE = "My Wardrobe"
 WINDOW_HEIGHT = 220
 WINDOW_WIDTH = 500
+IMG_WIDTH = 250
+IMG_HEIGHT = 250
 
 #store all Tops into a file and skip hidden files
-ALL_TOPS = [str('tops/') + imagefile for imagefile in os.listdir('tops/') if not file.startswith('.')]
+ALL_TOPS = [str('tops/') + imagefile for imagefile in os.listdir('tops/') if not imagefile.startswith('.')]
 
 class WardrobeApp:
     def __init__(self, root):
@@ -18,6 +20,16 @@ class WardrobeApp:
         #show top image in the window
         self.top_images = ALL_TOPS
 
+        #save single top - image filepath
+        self.top_image_path = self.top_images[0]
+        
+        #create and add top image into Frame
+        self.tops_frame = tk.Frame(self.root)
+        self.top_image_label = self.create_photo(self.top_image_path, self.tops_frame)
+
+        #add top to pack
+        self.top_image_label.pack(side=tk.TOP)
+        
         self.create_background()
 
 
@@ -25,6 +37,20 @@ class WardrobeApp:
         #add title to window
         self.root.title(WINDOW_TITLE)
         self.root.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}')
+
+        #add clothing
+        self.tops_frame.pack(fill=tk.BOTH, expand=tk.YES)
+
+    def create_photo(self, image_path, frame):
+        image_file = Image.open(image_path)
+        image_resized = image_file.resize((IMG_WIDTH, IMG_HEIGHT), Image.ANTIALIAS)
+        tk_photo = ImageTk.PhotoImage(image_resized)
+        image_label = tk.Label(frame, image=tk_photo, anchor=tk.CENTER)
+
+        image_label.image = tk_photo
+
+        return image_label
+
 
 root = tk.Tk()
 app = WardrobeApp(root)
